f576ed10611c0e73f5d2d33ef6f4bcfed84134a4
Michael Larkins <mjlarkins@gmail.com>
2026-02-11 19:58:50 +1000

    Fix missing stop_smtp_server export in smtp_server module

--- smtp_server/__init__.py
+++ smtp_server/__init__.py
@@ -1,7 +1,7 @@
 """SMTP Server Module for receiving emails."""
 
 from .handler import MailHandler
-from .server import start_smtp_server
+from .server import start_smtp_server, stop_smtp_server
 from .email_storage import store_email
 
-__all__ = ['MailHandler', 'start_smtp_server', 'store_email']
+__all__ = ['MailHandler', 'start_smtp_server', 'stop_smtp_server', 'store_email']
